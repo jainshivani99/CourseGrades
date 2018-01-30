fb864d741a5bb15c115a3e74a61a903aa9349d7b
Shivani Jain <jainshivani99@yahoo.com>
2018-01-30 01:12:35 -0600

    Added in comments in MultipleCourseGradesTest & ArrayCourseGradesTest classes

--- tests/ArrayCourseGradesTest.java
+++ tests/ArrayCourseGradesTest.java
@@ -1,16 +1,18 @@
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
+/**A class that loads all the JSON files provided and parses it into objects. */
 public class ArrayCourseGradesTest {
 
+    //Instantiates an array list that contains arrays of course grade objects
     private ArrayList<CourseGrades[]> myCourseGrades = new ArrayList<CourseGrades[]>();
 
+    //Uses a loop to parse content from several JSON files
+    //Use a similar process for several Json files (use a method and call it several times in a loop), returns an array of course grade objects
     @Before
     public void setUp() throws Exception {
         List<String> jsonFiles = Data.getJsonFilesAsList();
@@ -25,16 +27,16 @@ public class ArrayCourseGradesTest {
         }
     }
 
+    //Tests for the CRN variable in the first JSON file (Fall 2013), third course grades object
     @Test
     public void getCRN() {
-        assertEquals(41758, myCourseGrades.get(0)[0].getCRN());
+        assertEquals(47102, myCourseGrades.get(0)[2].getCRN());
     }
 
+    //Tests for the Subject variable in the first JSON file (Fall 2013), first course grades object
     @Test
     public void getSubject() {
         assertEquals("AAS", myCourseGrades.get(0)[0].getSubject());
     }
-
-    //Use a similar process for several Json files (use a method and call it several times in a loop), returns an array of course grade objects
 
 }
--- tests/MultipleCourseGradesTest.java
+++ tests/MultipleCourseGradesTest.java
@@ -1,26 +1,37 @@
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
 
+/**A test class that tests the functionality of multiple course grade objects that are subsequently stored in an array. */
 public class MultipleCourseGradesTest {
 
     private static final String COURSE_JSON =  "[{ \"CRN\": 47100, \"Subject\": \"AAS\", \"Number\": 100, \"Title\": \"Intro Asian American Studies\", \"Section\": \"AD2\", \"Type\": \"DIS\", \"Term\": 120138, \"Instructor\": \"Arai, Sayuri\", \"Grades\": [6, 11, 4, 5, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0], \"Average\": 3.64 },{ \"CRN\": 47102, \"Subject\": \"AAS\", \"Number\": 100, \"Title\": \"Intro Asian American Studies\", \"Section\": \"AD3\", \"Type\": \"DIS\", \"Term\": 120138, \"Instructor\": \"Davis, Thomas E\", \"Grades\": [2, 24, 1, 2, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0], \"Average\": 3.75 },{ \"CRN\": 51248, \"Subject\": \"AAS\", \"Number\": 100, \"Title\": \"Intro Asian American Studies\", \"Section\": \"AD4\", \"Type\": \"DIS\", \"Term\": 120138, \"Instructor\": \"Davis, Thomas E\", \"Grades\": [7, 16, 4, 4, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0], \"Average\": 3.71 }]";
     private CourseGrades[] myCourseGrades;
 
+    //Instantiates a Gson object and parses from the Json file into the variable myCourseGrades
     @Before
     public void setUp() throws Exception {
         Gson gson = new Gson();
         myCourseGrades = gson.fromJson(COURSE_JSON, CourseGrades[].class);
     }
 
+    //Tests CRN from the first course grade object
     @Test
-    public void getCRN() {
+    public void getCRNfromObject1() {
         assertEquals(47100, myCourseGrades[0].getCRN());
+    }
+
+    //Tests CRN from the second course grade object
+    @Test
+    public void getCRNfromObject2() {
         assertEquals(47102, myCourseGrades[1].getCRN());
+    }
+
+    //Tests CRN from the third course grade object
+    @Test
+    public void getCRNfromObject3() {
         assertEquals(51248, myCourseGrades[2].getCRN());
     }
 
