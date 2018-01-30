642e1990189775f24e7c384c38f047bfb544070c
Shivani Jain <jainshivani99@yahoo.com>
2018-01-30 00:17:30 -0600

    Finished parsing JSON from array list of files

--- tests/ArrayCourseGradesTest.java
+++ tests/ArrayCourseGradesTest.java
@@ -3,35 +3,38 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class ArrayCourseGradesTest {
 
-    private CourseGrades[] myCourseGrades;
-    private static final String COURSE_JSON = Data.getFileContentsAsString("Fall2013.json");
-    
+    private ArrayList<CourseGrades[]> myCourseGrades = new ArrayList<CourseGrades[]>();
+
     @Before
     public void setUp() throws Exception {
-        //Convert a Json file to a String by using the getFileContentsAsString() method from the Data class
-        //String courseJson = Data.getFileContentsAsString("Fall2013.json");
-        //Make a Gson object and set up a array of course grades objects
-        Gson gson = new Gson();
-        //Parse a single Json file into an array of course grades objects
-        myCourseGrades = gson.fromJson(COURSE_JSON, CourseGrades[].class);
+        List<String> jsonFiles = Data.getJsonFilesAsList();
+        for (int index = 0; index < jsonFiles.size(); index++) {
+            //Convert a Json file to a String by using the getFileContentsAsString() method from the Data class
+            String courseJson = Data.getFileContentsAsString(jsonFiles.get(index));
+            //Make a Gson object and set up a array of course grades objects
+            Gson gson = new Gson();
+            //Parse a single Json file into an array of course grades objects
+            CourseGrades[] courseObjects = gson.fromJson(courseJson, CourseGrades[].class);
+            myCourseGrades.add(courseObjects);
+        }
     }
 
     @Test
     public void getCRN() {
-        assertEquals(41758, myCourseGrades[0].getCRN());
+        assertEquals(41758, myCourseGrades.get(0)[0].getCRN());
     }
 
     @Test
     public void getSubject() {
-        assertEquals("AAS", this.myCourseGrades[0].getSubject());
+        assertEquals("AAS", myCourseGrades.get(0)[0].getSubject());
     }
 
     //Use a similar process for several Json files (use a method and call it several times in a loop), returns an array of course grade objects
-
 
 }
