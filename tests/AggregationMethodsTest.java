90fee10bdbb88d1e1410ed5f368f67ddafa54773
Shivani Jain <jainshivani99@yahoo.com>
2018-01-30 20:15:32 -0600

    Finished writing all 6 filtering methods

--- tests/AggregationMethodsTest.java
+++ tests/AggregationMethodsTest.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class AggregationMethodsTest {
+
+    public int numStudentsInClasses(ArrayList<CourseGrades> myCourseGrades) {
+
+    }
+
+    public int numStudentsWithGrade(ArrayList<CourseGrades> myCourseGrades) {
+
+    }
+
+    public double gpaMeanByCourse(ArrayList<CourseGrades> myCourseGrades) {
+
+    }
 }
--- tests/FilteringMethodsTest.java
+++ tests/FilteringMethodsTest.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 
+
 public class FilteringMethodsTest {
 
-    //Returns an array list of all courses that fit a certain
+    //Returns an array list of all courses that fit a given subject
     public ArrayList<CourseGrades> getCoursesFromSubject(ArrayList<CourseGrades> myCourseGrades, String subject) {
         ArrayList<CourseGrades> desiredCourseGrades = new ArrayList<CourseGrades>();
         for (int index = 0; index < myCourseGrades.size(); index++) {
@@ -14,35 +15,44 @@ public class FilteringMethodsTest {
         return desiredCourseGrades;
     }
 
-    //what about a fragment of the instructor's name
+    //Returns an array list of courses whose instructor matches the given instructor name or fragment of name
     public ArrayList<CourseGrades> getCoursesFromInstructor(ArrayList<CourseGrades> myCourseGrades, String instructor) {
         ArrayList<CourseGrades> desiredCourseGrades = new ArrayList<CourseGrades>();
         for (int index = 0; index < myCourseGrades.size(); index++) {
             CourseGrades currentCourse = myCourseGrades.get(index);
-            if (currentCourse.getInstructor().equals(instructor)){
+            if (currentCourse.getInstructor().contains(instructor)){
                 desiredCourseGrades.add(currentCourse);
             }
         }
         return desiredCourseGrades;
     }
 
-    //how to use a range, won't use .equals()
-    public ArrayList<CourseGrades> getCoursesFromNumber(ArrayList<CourseGrades> myCourseGrades, int range) {
+    //Returns an array list of courses whose course number is within a certain range
+    public ArrayList<CourseGrades> getCoursesFromNumber(ArrayList<CourseGrades> myCourseGrades, int low, int high) {
         ArrayList<CourseGrades> desiredCourseGrades = new ArrayList<CourseGrades>();
         for (int index = 0; index < myCourseGrades.size(); index++) {
             CourseGrades currentCourse = myCourseGrades.get(index);
-            if (currentCourse.getNumber() == range){
+            if (currentCourse.getNumber() >= low && currentCourse.getNumber() <= high){
                 desiredCourseGrades.add(currentCourse);
             }
         }
         return desiredCourseGrades;
     }
 
-    //what attribute represents number of students, won't use .equals()
-    public ArrayList<CourseGrades> getCoursesFromStudents(int students) {
-        return null;
+    //Returns an array list of courses whose number of students in within a certain range
+    //Assumption: Each element in a the grades array of a course object represents one student's grade, therefor the length of the array is the total number of students in the course
+    public ArrayList<CourseGrades> getCoursesFromStudents(ArrayList<CourseGrades> myCourseGrades, int low, int high) {
+        ArrayList<CourseGrades> desiredCourseGrades = new ArrayList<CourseGrades>();
+        for (int index = 0; index < myCourseGrades.size(); index++) {
+            CourseGrades currentCourse = myCourseGrades.get(index);
+            if ((currentCourse.getGrades()).length >= low && (currentCourse.getGrades()).length <= high){
+                desiredCourseGrades.add(currentCourse);
+            }
+        }
+        return desiredCourseGrades;
     }
 
+    //Returns an array list of courses that contain the given type
     public ArrayList<CourseGrades> getCoursesFromType(ArrayList<CourseGrades> myCourseGrades, String type) {
         ArrayList<CourseGrades> desiredCourseGrades = new ArrayList<CourseGrades>();
         for (int index = 0; index < myCourseGrades.size(); index++) {
@@ -54,6 +64,7 @@ public class FilteringMethodsTest {
         return desiredCourseGrades;
     }
 
+    //Returns an array list of courses that contain the given section
     public ArrayList<CourseGrades> getCoursesFromSection(ArrayList<CourseGrades> myCourseGrades, String section) {
         ArrayList<CourseGrades> desiredCourseGrades = new ArrayList<CourseGrades>();
         for (int index = 0; index < myCourseGrades.size(); index++) {
